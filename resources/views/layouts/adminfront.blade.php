0f0d5ba87bc855ead96a79d4a486c942ae610084
Servitechno <servitechno777@gmail.com>
2022-08-18 15:43:13 -0400

    cambios generales

--- resources/views/layouts/adminfront.blade.php
+++ resources/views/layouts/adminfront.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>TECHNOSYSTEMS - @yield('title')</title>
+    <title>MMMCATIA | @yield('title')</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta name="robots" content="noindex, nofollow">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
